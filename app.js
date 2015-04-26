2875fac339d92a39f2954ac7918de69ac53f9bcc
Kevin Lanzon <kevinlanzon@gmail.com>
2015-04-26 19:18:12 +0100

    updated travis file

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
@@ -12,10 +11,8 @@ app.get('/', function (req, res) {
 res.render('index');
 });
 
-
-
 app.listen(port, function () {
 
 console.log('App hopefully running at http://%s:%s', port);
 
-})
+});
